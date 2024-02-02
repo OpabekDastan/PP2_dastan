03c8fbafef4aff3761ff13a3bd21bd271f502495
dastan <dastanopabek08@gmail.com>
2024-02-02 23:03:02 +0600

    chickens

--- lab3/functions1/task03.py
+++ lab3/functions1/task03.py
@@ -1,2 +1,13 @@
-def headlegs();
+def solve(head,legs):
+    num_chicken=0
+    num_rabbits=0
+    for i in range(head + 1):
+        num_chicken=i 
+        num_rabbits=head-i
+        if((num_chicken*2) + (num_rabbits*4) == legs):
+            break
+
+    print(f"chicks = {num_chicken} ")
+    print(f"rabbits = {num_rabbits} ") 
+solve(35, 94)
     
